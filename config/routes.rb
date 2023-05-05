5469aa6f2832b8f53d939359a422b407f0c65580
Suzanne Hamilton <suzanne.hamilton@citizensadvice.org.uk>
2023-05-05 14:11:18 +0100

    Add status endpoint
    The `/status` endoint may be used by infrastructure healthchecks to
    determine whether a Rails app is healthy.
    
    I'm not sure whether it's needed for the search-api, but the pods
    currently won't deploy, and this may be one of the reasons why.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "status#index"
+  get "/status", to: "status#index"
 
   # Temporary route to check the app can connect to the database in AWS. This can
   # be deleted once we've added some real routes.
