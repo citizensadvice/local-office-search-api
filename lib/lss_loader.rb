dc60e248513c367f9c6f341cb3f0b6ac48857045
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2023-06-05 17:17:26 +0100

    NAT-213: parse geographi information

--- lib/lss_loader.rb
+++ lib/lss_loader.rb
@@ -1,22 +1,84 @@
 # frozen_string_literal: true
 
-module LssLoader
+class LssLoader
   # @param [CSV::Table] account_csv
   # @param [CSV::Table] opening_hours_csv
-  def self.update_offices(account_csv, opening_hours_csv)
-    ActiveRecord::Base.transaction do
-      raise LssLoadError, "Accounts CSV file was not in expected format" unless account_csv.headers == %w[
-        Id Name ParentId Serial_Number__c RecordTypeId About_our_advice_service__c Access_details__c
-        BillingStreet BillingCity BillingPostalCode BillingLatitude BillingLongitude Email__c Website Phone
-        Closing_Date__c Reopening_Date__c Closed__c
-        Recruiting_volunteers__c Select_the_roles_you_re_recruiting_for__c Volunteer_Recruitment_Email__c
-        Membership_Number__c Local_Office_Opening_Hours_Information__c Telephone_Advice_Hours_Information__c
-      ]
-      raise LssLoadError, "Opening Hours CSV file was not in expected format" unless opening_hours_csv.headers == %w[
-        Id Parent_Account__c RecordTypeId Weekday__c Start_Time__c End_Time__c Type__c
-      ]
+  def initialize(account_csv, opening_hours_csv)
+    @account_csv = account_csv
+    @opening_hours_csv = opening_hours_csv
+  end
 
+  def load!
+    ActiveRecord::Base.transaction do
+      validate_csv_headers!
       ActiveRecord::Base.connection.truncate(Office.table_name)
+
+      @account_csv.each do |row|
+        office = office_from_row row
+        office.save!
+      end
+    end
+  end
+
+  private
+
+  def validate_csv_headers!
+    raise LssLoadError, "Accounts CSV file was not in expected format" unless @account_csv.headers == %w[
+      Id Name ParentId Serial_Number__c RecordTypeId About_our_advice_service__c Access_details__c
+      BillingStreet BillingCity BillingPostalCode BillingLatitude BillingLongitude Email__c Website Phone
+      Closing_Date__c Reopening_Date__c Closed__c
+      Recruiting_volunteers__c Select_the_roles_you_re_recruiting_for__c Volunteer_Recruitment_Email__c
+      Membership_Number__c Local_Office_Opening_Hours_Information__c Telephone_Advice_Hours_Information__c
+    ]
+    raise LssLoadError, "Opening Hours CSV file was not in expected format" unless @opening_hours_csv.headers == %w[
+      Id Parent_Account__c RecordTypeId Weekday__c Start_Time__c End_Time__c Type__c
+    ]
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def office_from_row(row)
+    Office.new id: row["Id"],
+               name: row["Name"],
+               office_type: record_type_id_to_office_type(row["RecordTypeId"]),
+               legacy_id: str_or_nil(row["Serial_Number__c"]),
+               about_text: str_or_nil(row["About_our_advice_service__c"]),
+               street: str_or_nil(row["BillingStreet"]),
+               city: str_or_nil(row["BillingCity"]),
+               postcode: str_or_nil(row["BillingPostalCode"]),
+               location: point_wkt_or_nil(row["BillingLatitude"], row["BillingLongitude"]),
+               email: str_or_nil(row["Email__c"]),
+               website: str_or_nil(row["Website"]),
+               phone: str_or_nil(row["Phone"]),
+               opening_hours_information: str_or_nil(row["Local_Office_Opening_Hours_Information__c"]),
+               telephone_advice_hours_information: str_or_nil(row["Telephone_Advice_Hours_Information__c"])
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def str_or_nil(val)
+    case val
+    when "null"
+      nil
+    else
+      val
+    end
+  end
+
+  def point_wkt_or_nil(latitude, longitude)
+    return nil if latitude.nil? || longitude.nil?
+
+    "POINT(#{longitude} #{latitude})"
+  end
+
+  def record_type_id_to_office_type(record_type_id)
+    case record_type_id
+    when "0124K000000HyUGQA0"
+      :member
+    when "0124K0000000qqTQAQ"
+      :office
+    when "0124K0000000qqUQAQ"
+      :outreach
+    else
+      raise LssLoadError, "Unrecognised RecordTypeId #{record_type_id}"
     end
   end
 
--- spec/lss_loader/lss_loader_spec.rb
+++ spec/lss_loader/lss_loader_spec.rb
@@ -6,32 +6,117 @@ require "csv"
 
 RSpec.describe LssLoader do
   it "removes any offices no longer referenced in the data" do
-    id = SecureRandom.hex(9)
-    Office.create! id:, name: "Testtown Citizens Advice", office_type: "member"
+    id = create_a_single_office
 
-    load_from_fixtures "single", "empty"
+    load_from_fixtures "minimal", "empty"
 
     expect(Office.all.map(&:id)).not_to include id
   end
 
   it "does not remove any offices if an error occurs during load" do
-    id = SecureRandom.hex(9)
-    Office.create! id:, name: "Testtown Citizens Advice", office_type: "member"
+    id = create_a_single_office
 
     load_from_fixtures_with_error "corrupt", "empty"
 
     expect(Office.all.map(&:id)).to eq [id]
   end
 
+  it "loads a single record into the database with minimal fields" do
+    load_from_fixtures "minimal", "empty"
+
+    expect_single_record id: "0014K00000PcCA6QAN",
+                         name: "Citizens Advice Bristol",
+                         office_type: "member"
+  end
+
+  # rubocop:disable RSpec/ExampleLength
+  it "loads a single record into the database with all text fields populated" do
+    load_from_fixtures "all_strings_populated", "empty"
+
+    expect_single_record id: "0014K00000PcCA6QAN",
+                         name: "Citizens Advice Bristol",
+                         office_type: "office",
+                         legacy_id: 101_185,
+                         about_text: "About our advice service",
+                         accessibility_information: [],
+                         street: "48 Fairfax Street",
+                         city: "BRISTOL",
+                         postcode: "BS1 3BL",
+                         email: "cab@example.com",
+                         website: "https://www.example.com/",
+                         phone: "0181 811 8181",
+                         opening_hours_information: "We are open for drop-ins",
+                         telephone_advice_hours_information: "Please call to book an appointment"
+  end
+  # rubocop:enable RSpec/ExampleLength
+
+  it "handles multiline strings in the source file correctly" do
+    load_from_fixtures "multiline", "empty"
+
+    expect(Office.first.about_text).to eq "This detail is on\nMultiple lines."
+  end
+
+  it "handles location information correctly" do
+    load_from_fixtures "has_location", "empty"
+
+    # Think the lat and lon look backwards? that's because in GIS coordinates are expressed in
+    # x,y terms, not lat, lon. Therefore the lon comes first.
+    expect(Office.first.location).to eq RGeo::Cartesian.preferred_factory.point(1.18184, 51.07988)
+  end
+
+  it "loads in accessibility information as list"
+  it "correctly assigns telephone and opening hour numbers"
+
+  def create_a_single_office
+    id = SecureRandom.hex(9)
+    Office.create! id:, name: "Testtown Citizens Advice", office_type: "member"
+    id
+  end
+
   def load_from_fixtures(account_csv, opening_hours_csv)
-    LssLoader.update_offices CSV.read(File.expand_path("../fixtures/accounts/#{account_csv}.csv", File.dirname(__FILE__)), headers: true),
-                             CSV.read(File.expand_path("../fixtures/opening_hours/#{opening_hours_csv}.csv", File.dirname(__FILE__)),
-                                      headers: true)
+    lss_loader = LssLoader.new CSV.read(File.expand_path("../fixtures/accounts/#{account_csv}.csv", File.dirname(__FILE__)), headers: true),
+                               CSV.read(File.expand_path("../fixtures/opening_hours/#{opening_hours_csv}.csv", File.dirname(__FILE__)),
+                                        headers: true)
+    lss_loader.load!
   end
 
   def load_from_fixtures_with_error(account_csv, opening_hours_csv)
     expect do
       load_from_fixtures account_csv, opening_hours_csv
     end.to raise_error LssLoader::LssLoadError
+  end
+
+  def expect_single_record(vals)
+    vals = {
+      parent_id: nil,
+      legacy_id: nil,
+      about_text: nil,
+      accessibility_information: [],
+      street: nil,
+      city: nil,
+      postcode: nil,
+      location: nil,
+      email: nil,
+      website: nil,
+      phone: nil,
+      opening_hours_information: nil,
+      opening_hours_monday: nil,
+      opening_hours_tuesday: nil,
+      opening_hours_wednesday: nil,
+      opening_hours_thursday: nil,
+      opening_hours_friday: nil,
+      opening_hours_saturday: nil,
+      opening_hours_sunday: nil,
+      telephone_advice_hours_information: nil,
+      telephone_advice_hours_monday: nil,
+      telephone_advice_hours_tuesday: nil,
+      telephone_advice_hours_wednesday: nil,
+      telephone_advice_hours_thursday: nil,
+      telephone_advice_hours_friday: nil,
+      telephone_advice_hours_saturday: nil,
+      telephone_advice_hours_sunday: nil
+    }.update(vals)
+
+    expect(Office.first.attributes.symbolize_keys).to eq vals
   end
 end
