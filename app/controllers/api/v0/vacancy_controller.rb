35b2b664e7e59d7e5ef51ed5308d48ed88341cc1
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2023-07-05 15:24:01 +0100

    fetch individual volunteer role via EPiServer API

--- app/controllers/api/v0/vacancy_controller.rb
+++ app/controllers/api/v0/vacancy_controller.rb
@@ -3,8 +3,14 @@
 module Api
   module V0
     class VacancyController < BaseController
+      include Serialisers
+
       def get
-        head :not_implemented
+        office = Office.find(params[:id])
+        head :not_found if office.volunteer_roles.empty?
+        render json: vacancy_as_v0_json(office) unless office.volunteer_roles.empty?
+      rescue ActiveRecord::RecordNotFound
+        head :not_found
       end
 
       def list
