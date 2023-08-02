2284f6d410be439cc12d61b2109e2aac4ead95d5
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2023-08-02 15:39:04 +0100

    NAT-318: allow fuzzy searching, matching on local authority and LCA names
    Although most users search using their postcode, a smaller number of users will search based on a place name.
    In order to accomodate these, we can do a simple search matching both on LCA name and local authority name.
    
    Checking based on current usage data, we think this gets us to a high degree of coverage, without having to
    implement a full geocoding solution for MVP.

--- lib/office_search.rb
+++ lib/office_search.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 module OfficeSearch
-  def self.by_location(near, opts)
+  def self.by_location(near, opts = {})
     opts[:only_with_vacancies] ||= false
     opts[:only_in_same_local_authority] ||= false
 
-    location, local_authority_id = find_location(near)
-
-    [build_query(location, local_authority_id, opts), location]
+    exact_location_results = find_exact_location(near)
+    if exact_location_results.nil?
+      [by_fuzzy_location(near, opts), nil]
+    else
+      location, local_authority_id = exact_location_results
+      [build_query_from_location(location, local_authority_id, opts), location]
+    end
   end
 
   class UnknownLocationError < StandardError
@@ -33,16 +37,16 @@ module OfficeSearch
     end
   end
 
-  def self.find_location(near)
+  def self.find_exact_location(near)
     postcode = Postcode.normalise_and_find(near)
-    raise UnknownLocationError if postcode.nil?
+    return nil if postcode.nil?
     raise OutOfAreaError, :ni if postcode.northern_irish?
     raise OutOfAreaError, :scotland if postcode.scottish?
 
     [postcode.location, postcode.local_authority_id]
   end
 
-  def self.build_query(location, local_authority_id, opts)
+  def self.build_query_from_location(location, local_authority_id, opts)
     q = Office.all
     q = if opts[:only_in_same_local_authority]
           q.where(local_authority_id:)
@@ -52,5 +56,20 @@ module OfficeSearch
 
     q = q.where.not(volunteer_roles: []) if opts[:only_with_vacancies]
     q.order(Office.arel_table[:location].st_distance(location))
+  end
+
+  def self.by_fuzzy_location(near, opts)
+    fuzzy_query = build_fuzzy_query(near, opts)
+    raise UnknownLocationError if fuzzy_query.empty?
+
+    fuzzy_query
+  end
+
+  def self.build_fuzzy_query(near, opts)
+    office_with_local_authorities = Office.left_outer_joins(:local_authority)
+    q = office_with_local_authorities.where(Office.arel_table[:name].matches("%#{near}%"))
+    q = q.or(office_with_local_authorities.where(LocalAuthority.arel_table[:name].matches("%#{near}%")))
+    q = q.where.not(volunteer_roles: []) if opts[:only_with_vacancies]
+    q.limit(10)
   end
 end
